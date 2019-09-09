34ca902a38314c5a589938932693b01fb45cd957
Philippe <philippemtremblay@hotmail.com>
2019-09-08 20:14:05 -0400

    SpikeRewspawner presque fini
     prefab, animation

---
+++ Assets/Scripts/SpikeRespawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlatformerPro;
+using System;
+
+public class SpikeRespawner : Hazard {
+
+    public GameObject Character;
+    public GameObject Respawner;
+    
+
+
+    protected override void DoHit(Collider2D other)
+    {
+        base.DoHit(other);
+        StartCoroutine(DoMove());
+        
+    }
+
+    IEnumerator DoMove()
+    {
+        yield return new WaitForSeconds(0.8f);
+        Character.transform.position = Respawner.transform.position;
+    }
+}
