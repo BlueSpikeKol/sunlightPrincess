927382c7164520e1de723c2c3b7e2c18eed1165c
Philippe <philippemtremblay@hotmail.com>
2019-10-06 16:29:47 -0400

    respawn points réglé.

---
+++ Assets/RespawnPointPerso.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PlatformerPro;
+using UnityEngine;
+
+public class RespawnPointPerso : RespawnPoint
+{
+    public string direction;
+
+	// Use this for initialization
+	void Awake() {
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
